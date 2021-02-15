2791e95ad3411d05819260ec2209079fdcf793d4
hashwnath <53908470+hashwnath@users.noreply.github.com>
2021-02-15 20:06:46 +0530

    Update studentCorner_NITJ.py

--- studentCorner_NITJ.py
+++ studentCorner_NITJ.py
@@ -1,4 +1,5 @@
 import requests
+import smtplib
 from bs4 import BeautifulSoup
 scwp = requests.get('https://www.nitj.ac.in/#section-2')
 soup = BeautifulSoup(scwp.content, 'html.parser')
@@ -18,15 +19,36 @@ fir = stclst[0]
 stclst = [i[2:] for i in stclst[1:]]   # ultimate student corner list.(sclst)
 stclst.insert(0,fir)
 # print(len(stclst))                  #print of stclst
-# print(stclst)
-prev = 'Notice regarding Registration in Google Classroom | '    #this was on top at the time of app making
-new = stclst[0]
+prev="Additional Information regarding |Registration of PG Students| in the 2nd Semester | "
+new=stclst[0]
+#email-automation-starts
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login('rishabcandy@gmail.com', 'umarani19')
+#---
 if(prev!=new):
     print('You have got a new message on student corner.')
     print(stclst[0])
-    prev = seplst[0]
+    #prev = seplst[0]
+    server.sendmail('rishabcandy@gmail.com', 's.hashwanth531@gmail.com',stclst[0])
+    #prev = seplst[0]
 else:
-    print('No new information.')    
-print('press any key to exit')
-exitkey = input()
+    print('No new information.')     
+def updateCount():
+    x=y='"'
+    fin = open('main.py', 'r')
+    code = fin.read()
+    fin.close()
+    second_line = code.split('\n')[21]
+    second_line_parts = second_line.split('=')
+    second_line_parts[1] = x+stclst[0][:]+y
+    
+    second_line = '='.join(second_line_parts)
+    lines = code.split('\n')
+    lines[21] = second_line
+    code = '\n'.join(lines)
 
+    fout = open('main.py', 'w')
+    fout.write(code)
+    fout.close()      
+updateCount()
